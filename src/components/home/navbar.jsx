bfb08ad4d66bbd54382a5b36d216ad36045db283
Jamini <jamini.18@cse.mrt.ac.lk>
2021-07-24 23:26:21 +0530

    home navbar-start

--- src/components/home/navbar.jsx
+++ src/components/home/navbar.jsx
@@ -1,68 +1,46 @@
-import React,{useState,useEffect} from 'react';
-import { Button } from './button';
+import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
+import card from '../../assets/svg/popup/credit-card.svg'
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button,setButton]=useState(true);
-
-    const handleClick =()=>  setClick(!click);
-    const closeMobileMenu=()=> setClick(false); 
-
-    const showButton =()=>{
-        if(window.innerWidth<=960){
-            setButton(false)
-        }else{
-            setButton(true)
-        }
-    };
-
-    useEffect(()=>{
-        showButton();
-    },[]);
-
-    window.addEventListener('resize', showButton);
+    const closeMobileMenu=()=> setClick(false);
 
     return (
-        <>
-            <nav className="navbar">
+        <body class="grid justify-items-end space-y-12 ">
+            <div className="bg-primary w-1/4 md:w-1/4 flex justify-center">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        TRVL <i className='fab fa-typo3' />
-                    </Link>
-                    <div className='menu-icon' onClick = {handleClick}>
-                        <i className={click ? 'fas fa-times':'fas fa-bars'} />
-                    </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                                Home
+                        <li className='bg-white w-1/4 md:w-full flex'>
+                            
+                            <Link to='/' className='' onClick={closeMobileMenu}>
+                                <div class="flex justify-between">
+                                <img width={32} src={card} alt="card" />
+                                <h1>notification</h1>
+                                </div>
+                                
                             </Link>
-                        </li>
+                        </li><br></br>
 
                         <li className='nav-item'>
                             <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                                Services
+                                Order history
                             </Link>
                         </li>
+                        <br></br>
 
                         <li className='nav-item'>
                             <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
-                                Products
+                                Night Mode
                             </Link>
                         </li>
 
-                        <li className='nav-item'>
-                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
-                                SignUp
-                            </Link>
-                        </li>
+                        
 
                     </ul>
-                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
                 </div>
-            </nav>
-        </>
+            </div>
+        </body>
     )
 }
 
