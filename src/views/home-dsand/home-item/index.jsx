9444884e3153271d3e0c3e2457adae741de33165
Darshana Sandaruwan <darshana.18@cse.mrt.ac.lk>
2021-07-25 20:04:31 +0530

    font sizes are responsive

--- src/views/home-dsand/home-item/index.jsx
+++ src/views/home-dsand/home-item/index.jsx
@@ -13,13 +13,13 @@ export default function HomeItem(props) {
 
     return (
         <div>
-            <div className="w-full h-60 bg-center bg-cover rounded-xl" style={{backgroundImage: `url(${comProps.image})`}} />
-            <div className="text-lg font-medium">{`${comProps.vendor} - ${comProps.road}`}</div>
-            <div>{comProps.city}</div>
+            <div className="w-full h-40 bg-center bg-cover rounded-xl" style={{backgroundImage: `url(${comProps.image})`}} />
+            <div className="text-sm lg:text-lg font-medium">{`${comProps.vendor} - ${comProps.road}`}</div>
+            <div className="text-xs lg:text-base">{comProps.city}</div>
             <div>
-                <span className="font-medium">{comProps.price}</span>
-                <span>
-                    <i className="fa fa-biking text-success text-2xl ml-4 mr-1"/>
+                <span className="font-medium text-xs lg:text-base">{comProps.price}</span>
+                <span className="text-xs lg:text-base">
+                    <i className="fa fa-biking text-success text-xs lg:text-base ml-4 mr-1"/>
                     {comProps.time}
                 </span>
             </div>
