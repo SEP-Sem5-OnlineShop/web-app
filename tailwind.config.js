fc3f178da95a29e805e74df4eb06d21c37b627e5
Darshana Sandaruwan <darshana.18@cse.mrt.ac.lk>
2021-07-25 19:37:30 +0530

    home basic is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,9 @@ module.exports = {
       xl: '1440px',
     },
     colors: {
-      primary: '#FFD600',
+      primary: '#E8DFD9',
       secondary: '#373737',
+      textLight: '#264A75',
       text: '#838383',
       link: '#FF7000',
       accent: '#E5E5E5',
