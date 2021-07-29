62934d7c66f77d5a17742885434db1f81abc599f
danukadj <danukasandaruwandj@gmail.com>
2021-07-29 21:27:12 +0530

    alert responsive

--- src/views/app/alert.jsx
+++ src/views/app/alert.jsx
@@ -5,7 +5,7 @@ const Alert = () => {
     return (
         <div className="px-2 py-4 sm:px-12 sm:py-12">
             <h1 className="text-3xl text-secondary flex flex-col">Alerts</h1>
-            <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+            <div className="mt-4 sm:mt-8 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 lg:gap-10">
                     <AlertComponent />
                     <AlertComponent />
                     <AlertComponent />
--- src/views/app/alertComponent.jsx
+++ src/views/app/alertComponent.jsx
@@ -1,15 +1,15 @@
 const AlertComponent = () => {
     return (
-        <div className="flex justify-between rounded-2xl overflow-hidden shadow-md bg-white h-40 sm:h-36" >
-            <img src="img/item1.png" alt="item1" className="h-40 w-36 sm:h-36"/>
-            <div className="mx-2 my-4 flex flex-col justify-between items-center">
+        <div className="flex justify-between rounded-2xl overflow-hidden shadow-md bg-white h-28 sm:h-36" >
+            <img src="img/item1.png" alt="item1" className="h-full w-20 sm:w-36 sm:h-36 object-cover"/>
+            <div className="mx-2 my-2 flex flex-col justify-between items-center">
                 <a className="text-base sm:text-xl text-secondary font-semibold" href="/vendor/product1">Burger with some</a>
                 <a className="text-sm sm:text-lg text-secondary" href="/vendor">Yummy Bakers</a>
-                <div className="mt-1 flex flex-row justify-between items-center">
+                <div className="sm:mt-1 flex flex-row justify-between items-center">
                     <button className="rounded-xl shadow w-8 h-8 sm:w-10 sm:h-10 flex justify-center items-center bg-white transform hover:scale-105 hover:shadow-md transition ease-out duration-400">
                         <span className="text-3xl sm:text-4xl text-text">-</span>
                     </button>
-                    <span className="m-2 text-lg sm:text-xl text-secondary">10</span>
+                    <span className="mx-1 text-lg sm:text-xl text-secondary">10</span>
                     <button className="rounded-xl shadow w-8 h-8 sm:w-10 sm:h-10 flex justify-center items-center bg-white transform hover:scale-105 hover:shadow-md transition ease-out duration-400">
                         <span className="text-3xl sm:text-4xl text-text">+</span>
                     </button>
