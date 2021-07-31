280f25aa887fc850f21761fb11ee7bb93ba39ed0
Darshana Sandaruwan <darshana.18@cse.mrt.ac.lk>
2021-07-31 21:41:25 +0530

    update 2

--- src/views/app/profile/index.jsx
+++ src/views/app/profile/index.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Profile() {
     return (
         <React.Fragment>
+            <div>
+                <div className="">
 
+                </div>
+            </div>
         </React.Fragment>
     )
 }
