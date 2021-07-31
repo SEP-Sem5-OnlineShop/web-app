9ac5e0fa0cb07fc8a887df1ac5898784f6e4bf66
danukadj <danukasandaruwandj@gmail.com>
2021-07-31 20:04:28 +0530

    alert list

--- src/views/app/alert.jsx
+++ src/views/app/alert.jsx
@@ -1,7 +1,36 @@
 import AlertComponent from './alertComponent';
 import logo from "../../assets/svg/logo/logo-264A75.svg"
+import { useState, useEffect } from 'react';
 
 const Alert = () => {
+    const [alerts, setAlerts] = useState([
+        {itemname: 'Burger with some', vendor: 'Yummy Bakers', image: 'img/item1.png', price: 100.00, id: 1 },
+        {itemname: 'Pizza saf sff', vendor: 'Sammy Bakers', image: 'img/item1.png', price: 500.00, id: 2 },
+        {itemname: 'Dasd dsfs sfs', vendor: 'Leo Bakers', image: 'img/item1.png', price: 60.00, id: 3 },
+        {itemname: 'Bread dfs fsd', vendor: 'Asta Bakers', image: 'img/item1.png', price: 80.00, id: 4 },
+        {itemname: 'Fgggd hfdh jhg', vendor: 'Yuno Bakers', image: 'img/item1.png', price: 70.00, id: 5 },
+        {itemname: 'Ydfh sdjkf ds', vendor: 'Luck Bakers', image: 'img/item1.png', price: 150.00, id: 6 },
+        {itemname: 'Hdffd fds gd', vendor: 'Magna Bakers', image: 'img/item1.png', price: 90.00, id: 7 },
+        {itemname: 'Jdgs kks dij', vendor: 'Charmi Bakers', image: 'img/item1.png', price: 120.00, id: 8 },
+    ]);
+
+    // const [alerts, setAlerts] = useState(null);
+
+    // useEffect(() => {
+    //     fetch('http://localhost:8000/customer/customer1/alerts')
+    //       .then(res => {
+    //           return res.json();
+    //       })
+    //       .then(data => {
+    //           setAlerts(data)
+    //       });
+    // }, []);
+
+    const handleRemove = (id) => {
+        const newAlerts = alerts.filter(alert => alert.id !== id);
+        setAlerts(newAlerts);
+    };
+
     return (
         /**
          * header test remove later
@@ -15,16 +44,15 @@ const Alert = () => {
                 <div className="w-full bg-white rounded-t-3xl lg:rounded-t-6xl relative flex flex-col" style={{minHeight: 'calc(100vh - 11rem)'}}>
 
 
+                    {/* alert component */}
                     <div className="px-2 py-4 sm:px-12 sm:py-12">
                         <h1 className="text-3xl text-secondary flex flex-col">Alerts</h1>
                         <div className="mt-4 sm:mt-8 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 lg:gap-10">
-                                <AlertComponent />
-                                <AlertComponent />
-                                <AlertComponent />
-                                <AlertComponent />
-                                <AlertComponent />
-                                <AlertComponent />
-                                <AlertComponent />
+                            {alerts && <>
+                                {alerts.map((alert) => (
+                                    <AlertComponent alert={alert} handleRemove={handleRemove} key={alert.id} />
+                                ))}
+                            </>}
                         </div>
                     </div>
 
--- src/views/app/alertComponent.jsx
+++ src/views/app/alertComponent.jsx
@@ -1,30 +1,39 @@
-const AlertComponent = () => {
-    const item = 'Burger with some';
-    const vendor = 'Yummy Bakers';
-    const image = 'img/item1.png';
-    let count = 10;
-    let price = '1200.00';
+import { useState } from "react";
+
+const AlertComponent = ({ alert, handleRemove }) => {
+
+    const [count, setCount] = useState(1);
+
+    const handleIncrease = (e) => {
+        setCount(count+1);
+    };
+    const handleDecrease = (e) => {
+        if(count>1){
+            setCount(count-1);
+        }
+    };
+
     return (
         <div className="flex justify-between rounded-2xl overflow-hidden shadow-md bg-white h-28 sm:h-36 transform hover:scale-105 hover:shadow-lg transition ease-out duration-400" >
-            <img src={ image } alt="" className="h-full w-20 sm:w-36 sm:h-36 object-cover"/>
+            <img src={ alert.image } alt="" className="h-full w-20 sm:w-36 sm:h-36 object-cover"/>
             <div className="mx-2 my-2 flex flex-col justify-between items-center">
-                <a className="text-base sm:text-xl text-secondary font-semibold" href="/vendor/product1">{ item }</a>
-                <a className="text-sm sm:text-lg text-secondary" href="/vendor">{ vendor }</a>
+                <a className="text-base sm:text-xl text-secondary font-semibold" href="/vendor/product1">{ alert.itemname }</a>
+                <a className="text-sm sm:text-lg text-secondary" href="/vendor">{ alert.vendor }</a>
                 <div className="sm:mt-1 flex flex-row justify-between items-center">
-                    <button className="rounded-xl shadow w-8 h-8 sm:w-10 sm:h-10 flex justify-center items-center bg-white transform hover:scale-105 hover:shadow-md transition ease-out duration-400" onClick=''>
+                    <button className="rounded-xl shadow w-8 h-8 sm:w-10 sm:h-10 flex justify-center items-center bg-white transform hover:scale-105 hover:shadow-md transition ease-out duration-400" onClick={ handleDecrease }>
                         <span className="text-3xl sm:text-4xl text-text">-</span>
                     </button>
                     <span className="mx-1 text-lg sm:text-xl text-secondary">{ count }</span>
-                    <button className="rounded-xl shadow w-8 h-8 sm:w-10 sm:h-10 flex justify-center items-center bg-white transform hover:scale-105 hover:shadow-md transition ease-out duration-400" onClick=''>
+                    <button className="rounded-xl shadow w-8 h-8 sm:w-10 sm:h-10 flex justify-center items-center bg-white transform hover:scale-105 hover:shadow-md transition ease-out duration-400" onClick={ handleIncrease }>
                         <span className="text-3xl sm:text-4xl text-text">+</span>
                     </button>
                 </div>
             </div>
             <div className="mr-1 my:4 sm:mx-4 flex flex-col items-end justify-between">
-                <button className="rounded-xl shadow w-10 h-10 sm:w-12 sm:h-12 flex justify-center items-center bg-white transform hover:scale-110 hover:shadow-md transition ease-out duration-400" onClick=''>
+                <button className="rounded-xl shadow w-10 h-10 sm:w-12 sm:h-12 flex justify-center items-center bg-white transform hover:scale-110 hover:shadow-md transition ease-out duration-400" onClick={() => handleRemove(alert.id)}>
                     <span className="text-3xl sm:text-4xl text-danger">x</span>
                 </button>
-                <div className="text-base sm:text-xl text-secondary flex justify-center">{ price }</div>
+                <div className="text-base sm:text-xl text-secondary flex justify-center">{ alert.price*count }</div>
             </div>
         </div>
     );
