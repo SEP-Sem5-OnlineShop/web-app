9fb9bafec80c36cb7e58828b175ca364da5e985d
Darshana Sandaruwan <darshana.18@cse.mrt.ac.lk>
2021-07-31 20:53:27 +0530

    layout added

---
+++ src/router/app.jsx
@@ -0,0 +1,29 @@
+import {
+    Switch,
+    useRouteMatch,
+    Route,
+} from "react-router-dom";
+import PaymentPopup from '../views/app/paymentPopup'
+import ThankPopup from '../views/app/thankPopup'
+import History from '../views/app/history'
+import Alert from '../views/app/alert'
+
+export default function AppRouter() {
+    const match = useRouteMatch()
+    return (
+        <Switch>
+            <Route path={`${match.path}/paymentPopup`}>
+                <PaymentPopup />
+            </Route>
+            <Route path={`${match.path}/thankPopup`}>
+                <ThankPopup />
+            </Route>
+            <Route path={`${match.path}/history`}>
+                <History />
+            </Route>
+            <Route path={`${match.path}/alert`}>
+                <Alert />
+            </Route>
+        </Switch>
+    )
+}
