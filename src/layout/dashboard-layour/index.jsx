edb9000a8681796b5b10070c12904aa63a897999
Darshana Sandaruwan <darshana.18@cse.mrt.ac.lk>
2021-09-28 11:03:10 +0530

    layout fixes

--- src/layout/dashboard-layour/index.jsx
+++ src/layout/dashboard-layour/index.jsx
@@ -99,7 +99,7 @@ export default function DashboardLayout(props) {
                         </div>
                     </div>
 
-                    <div className="bg-white relative p-8"
+                    <div className="w-full bg-white relative p-8"
                         style={childStyle}>
                         {props.children}
                     </div>
