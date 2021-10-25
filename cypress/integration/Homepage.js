d8545dc4ee314a4e7e72c58c97c472f730816d43
danukadj <danukasandaruwandj@gmail.com>
2021-10-25 20:29:11 +0530

    fix

--- cypress/integration/Homepage.js
+++ cypress/integration/Homepage.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 describe('Homepage', () => {
     it('redender correctly', () => {
         //go to home page
--- cypress/integration/Vendorpage.js
+++ cypress/integration/Vendorpage.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 describe('Vendorpage', () => {
     
     it('redender correctly', () => {
