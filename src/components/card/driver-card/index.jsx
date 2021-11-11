e011e90b1e4814e2faec504f8da649099041dc93
danukadj <danukasandaruwandj@gmail.com>
2021-11-11 20:09:57 +0530

    homepage categories

--- src/components/card/driver-card/index.jsx
+++ src/components/card/driver-card/index.jsx
@@ -1,4 +1,4 @@
-import {getFileUrl} from "../../../api/azure-storage-blob";
+// import {getFileUrl} from "../../../api/azure-storage-blob";
 import { Link } from "react-router-dom";
 
 export default function DriverCard({ data, vendor_id }) {
@@ -6,7 +6,7 @@ export default function DriverCard({ data, vendor_id }) {
         <div className="bg-gray-200 font-sans h-full w-full flex flex-row justify-center items-center">
             <div className="card w-96 mx-auto bg-primary rounded-xl shadow-xl hover:shadow py-4">
                 <img className="w-32 mx-auto rounded-full border-8 border-white"
-                     src={getFileUrl(data.imageUrl || "")} alt="" />
+                     src={""} alt="" />
                 <Link to={`/vendor_${vendor_id}/driver_${data._id}`}>
                 <div className="text-center mt-2 text-sm xs:text-base sm:text-lg md:text-xl font-medium">{`${data.firstName || ""} ${data.lastName || ""}`}</div>
                 </Link>
