1954643fdb171ef1abc9c0af12c14a032887f870
Darshana Sandaruwan <darshana.18@cse.mrt.ac.lk>
2021-11-11 22:12:25 +0530

    socket url was changed

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://20.102.65.167:8000"
+const URL = "https://ontheway.thilinatlm.live/"
 // const URL = "http://localhost:8000"
 const socket = io(URL, { autoConnect: false });
 export const alertSocket = io(`${URL}/alert`,{ autoConnect: false })
